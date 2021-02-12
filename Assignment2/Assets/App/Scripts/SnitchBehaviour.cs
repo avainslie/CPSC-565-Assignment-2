3af6389178a5dba208474a704430bec030d782b9
avainslie <50717419+avainslie@users.noreply.github.com>
2021-02-12 14:42:32 -0700

    Snitch slowly flys randomly

---
+++ Assignment2/Assets/App/Scripts/SnitchBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnitchBehaviour : MonoBehaviour
+{
+
+    private Rigidbody rigidbody;
+    public int seed;
+
+    private System.Random rng;
+
+    private void Awake()
+    {
+        rng = new System.Random(seed);   
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // Set rigid body to the one on the snitch in the scene
+        rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // NextDouble returns random float between 0.0 and 1.0
+        // Taken from CPSC 565 - Lecture 8
+        Vector3 forceDir = new Vector3((float)(rng.NextDouble() * 2) - 1, (float)(rng.NextDouble() * 2) - 1, (float)(rng.NextDouble()*2) - 1);
+
+        // 
+        rigidbody.AddForce(forceDir);
+    }
+}
