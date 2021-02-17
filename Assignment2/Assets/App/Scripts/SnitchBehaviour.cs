f03325564e455fe93fe20139855b50eca540543b
avainslie <50717419+avainslie@users.noreply.github.com>
2021-02-16 17:25:21 -0700

    Update snitch movement
    Still a little strange. Not exactly smooth and the pivot point is weird (not entirely matching the empty game object, I think, for some reason)

--- Assignment2/Assets/App/Scripts/SnitchBehaviour.cs
+++ Assignment2/Assets/App/Scripts/SnitchBehaviour.cs
@@ -4,34 +4,115 @@ using UnityEngine;
 
 public class SnitchBehaviour : MonoBehaviour
 {
+    // Can be set by user
+    [SerializeField] private int seed;
+    [SerializeField] private float maxVelocity;
+    [SerializeField] private float minVelocity;
 
     private Rigidbody rigidbody;
-    public int seed;
-
     private System.Random rng;
+    private Vector3 oldForceDir;
+    private bool liftOff;
 
+    /****** PHYSICS 101 NOTES ******
+    
+    force = mass * acceleration
+    
+    acceleration = (final velocity - initial velocity) / change in time
+
+    momentum = mass * velocity 
+
+    velocity = displacement / change in time
+
+    *******************************/
+
+
+    // Only called once (before Start())
     private void Awake()
-    {
-        rng = new System.Random(seed);   
+    {   
+        rng = new System.Random(seed);  
+        // An idea for later
+        //Random.seed = System.DateTime.Now.Millisecond;
+        // https://answers.unity.com/questions/1606295/transformrotate-doesnt-work-inside-start-method.html 
+
+
+        oldForceDir = Vector3.up * 5;
+        liftOff = false;
     }
 
     // Start is called before the first frame update
+    // TODO: make it so snitch gradually comes up, not just starts floating
     void Start()
     {
+
+        // Make snitch move up first before moving randomly
+        transform.Translate(oldForceDir);
+
+
+        
+        
 
         // Set rigid body to the one on the snitch in the scene
         rigidbody = GetComponent<Rigidbody>();
 
+        
+
     }
+
+  
 
     // Update is called once per frame
     void Update()
     {
-        // NextDouble returns random float between 0.0 and 1.0
+        // https://answers.unity.com/questions/1606295/transformrotate-doesnt-work-inside-start-method.html
+        // if (!liftOff){
+        //     if (rng.NextDouble() < 0.5){ transform.Rotate(Vector3.right * Time.deltaTime * 400); }
+        //     else{ transform.Rotate(Vector3.left * Time.deltaTime * 300); } 
+            
+        //     //rigidbody.AddForce(transform.TransformDirection(oldForceDir) * 20);
+        //     liftOff = true;
+        // }
+
+
+        //// A little weird movement, but random and fairly quick (can be sped up...more jerky at faster speeds)
+
+        // NextDouble returns random float between 0.0 and 1.0, do math to get values between -1.0 and 1.0
         // Taken from CPSC 565 - Lecture 8
         Vector3 forceDir = new Vector3((float)(rng.NextDouble() * 2) - 1, (float)(rng.NextDouble() * 2) - 1, (float)(rng.NextDouble()*2) - 1);
 
-        // 
-        rigidbody.AddForce(forceDir);
+        // Add the random Vector3 to the snitch's physics if it was not the last move the snitch made
+        // Inspired by Omar's code 
+        // https://github.com/omaddam/Boids-Simulation/blob/develop/Assets/Boids/Scripts/Bird.cs
+        if (forceDir != oldForceDir){
+        
+            // Initialize the new velocity
+            Vector3 acceleration = Vector3.zero;
+
+            // Compute alignment
+            acceleration += forceDir * 2;
+
+            // Compute the new velocity
+            Vector3 velocity = rigidbody.velocity;
+            velocity += acceleration * Time.deltaTime;
+
+            // Ensure the velocity remains within the accepted range
+            velocity = velocity.normalized * Mathf.Clamp(velocity.magnitude,
+                minVelocity, maxVelocity);
+
+            // Apply velocity
+            rigidbody.velocity = velocity;
+
+            // Update rotation
+            transform.forward = rigidbody.velocity.normalized;
+
+            //rigidbody.AddForce(forceDir * 15);
+            oldForceDir = forceDir;
+        }  
+
     }
+
+
+
+
+
 }
